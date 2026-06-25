c949c0cf16f3a6509b5f6ba0bdb1773c9055a93c
0x-Shivam <ny753339@gmail.com>
2026-06-25 10:39:59 +0530

    removing local host ip and adding cloude needed config

--- app.py
+++ app.py
@@ -80,5 +80,7 @@ def start_scan():
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    print("[+] Starting Web Dashboard on http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    print(f"[+] Starting Web Dashboard on port {port}")
+    app.run(host='0.0.0.0', port=port, debug=False)
